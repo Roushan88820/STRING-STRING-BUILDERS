4b49a4d3e0c663a56ddae0788c1a625bd3b8c092
Roushan88820 <95723776+Roushan88820@users.noreply.github.com>
2022-02-12 05:20:54 +0530

    Update print all palindromic substrings.java

--- print all palindromic substrings.java
+++ print all palindromic substrings.java
@@ -1,3 +1,40 @@
+                                                                                    QUESTION
+Print All Palindromic Substrings
+Easy
+
+1. You are given a string. 
+2. You have to print all palindromic substrings of the given string.
+
+Constraints
+1 <= length of string <= 500
+
+Format
+Input
+
+A String
+
+Output
+
+All palindromic substrings(one in a line).
+First, all palindromic substrings starting from first character of string will be printed, then from second character and so on.
+
+Example
+
+Sample Input
+
+abcc
+
+Sample Output
+a
+b
+c
+cc
+c                                                                                    
+
+
+*********************************************************************************************************************************************************************************
+                                                                                     {CODE}
+
 import java.util.*;
 import java.io.*;
 public class Main {
@@ -49,3 +86,20 @@ public class Main {
     }
 
 }
+
+*********************************************************************************************************************************************************************************
+                                                                                  OUTPUT
+a
+b
+c
+cc
+c
+                                                                                                                                                                                                                                                 
+*********************************************************************************************************************************************************************************
+                                                                                  Kyaa Sikheee
+                                                                                  
+                                                                                  
+
+
+
+                            
